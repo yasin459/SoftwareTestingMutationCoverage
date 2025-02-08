2fbc42bdca18f5ed90230ea5583c9e338fdc57bb
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2025-02-08 22:00:02 +0330

    add more tests

--- tests.py
+++ tests.py
@@ -5,6 +5,19 @@ import os
 
 class TestCircuitSimulator(unittest.TestCase):
 
+    def run_isc_to_bench_test(self, input_file, output_file, expected_output_file):
+        phaseOne.isc_to_bench(input_file, output_file)
+
+        with open(expected_output_file, "r") as f:
+            expected_bench = f.read()
+
+        with open(output_file, "r") as f:
+            actual_bench = f.read()
+
+        self.assertEqual(actual_bench.strip(), expected_bench.strip())
+
+        os.remove(output_file)
+
     def test_check_all_number(self):
         self.assertTrue(phaseOne.check_all_number("123 456 789"))
         self.assertFalse(phaseOne.check_all_number("123 ABC 789"))
@@ -85,67 +98,72 @@ class TestCircuitSimulator(unittest.TestCase):
         os.remove(pi_file)
         os.remove(output_file)
 
+    def test_isc_to_bench_c5(self):
+        input_file = "./testFiles/c5.isc"
+        output_file = "c5.bench"
+        expected_output_file = "./testFiles/c5.bench"
+        self.run_isc_to_bench_test(input_file, output_file, expected_output_file)
+
     def test_isc_to_bench_c17(self):
-        input_file = "test_c17.isc"
-        output_file = "test_c17.bench"
+        input_file = "./testFiles/c17.isc"
+        output_file = "c17.bench"
+        expected_output_file = "./testFiles/c17.bench"
+        self.run_isc_to_bench_test(input_file, output_file, expected_output_file)
 
-        isc_content = """1     1gat inpt    1   0      >sa1
-    2     2gat inpt    1   0      >sa1
-    3     3gat inpt    2   0 >sa0 >sa1
-    8     8fan from     3gat      >sa1
-    9     9fan from     3gat      >sa1
-    6     6gat inpt    1   0      >sa1
-    7     7gat inpt    1   0      >sa1
-   10    10gat nand    1   2      >sa1
-     1     8
-   11    11gat nand    2   2 >sa0 >sa1
-     9     6
-   14    14fan from    11gat      >sa1
-   15    15fan from    11gat      >sa1
-   16    16gat nand    2   2 >sa0 >sa1
-     2    14
-   20    20fan from    16gat      >sa1
-   21    21fan from    16gat      >sa1
-   19    19gat nand    1   2      >sa1
-    15     7
-   22    22gat nand    0   2 >sa0 >sa1
-    10    20
-   23    23gat nand    0   2 >sa0 >sa1
-    21    19"""
+    def test_isc_to_bench_c432(self):
+        input_file = "./testFiles/c432.isc"
+        output_file = "c432.bench"
+        expected_output_file = "./testFiles/c432.bench"
+        self.run_isc_to_bench_test(input_file, output_file, expected_output_file)
 
-        expected_bench = """#test_c17
-#5 inputs
-#2 outputs
-#0 inverters
-#6 gates ( 6 NANDs )
+    def test_isc_to_bench_c499(self):
+        input_file = "./testFiles/c499.isc"
+        output_file = "c499.bench"
+        expected_output_file = "./testFiles/c499.bench"
+        self.run_isc_to_bench_test(input_file, output_file, expected_output_file)
 
-INPUT(1)
-INPUT(2)
-INPUT(3)
-INPUT(6)
-INPUT(7)
+    def test_isc_to_bench_c880(self):
+        input_file = "./testFiles/c880.isc"
+        output_file = "c880.bench"
+        expected_output_file = "./testFiles/c880.bench"
+        self.run_isc_to_bench_test(input_file, output_file, expected_output_file)
 
-OUTPUT(22)
-OUTPUT(23)
+    def test_run_bench_c5(self):
+        bench_file_out = "./testFiles/c5.bench"
+        input_wire_in = "./testFiles/c5.pi"
+        expected_output_file = "./testFiles/c5.log"
 
-10 = NAND(1, 3)
-11 = NAND(3, 6)
-16 = NAND(2, 11)
-19 = NAND(11, 7)
-22 = NAND(10, 16)
-23 = NAND(16, 19)"""
+        output_file = "c5.log"
 
-        with open(input_file, "w") as f:
-            f.write(isc_content)
+        phaseOne.run_bench(bench_file_out, input_wire_in, output_file)
 
-        phaseOne.isc_to_bench(input_file, output_file)
+        with open(expected_output_file, "r") as f:
+            expected_bench = f.read()
 
-        self.assertTrue(os.path.exists(output_file))
         with open(output_file, "r") as f:
-            content = f.read()
-            self.assertEqual(content.strip(), expected_bench.strip())
+            actual_bench = f.read()
 
-        os.remove(input_file)
+        self.assertEqual(actual_bench.strip(), expected_bench.strip())
+
+        os.remove(output_file)
+
+    def test_run_bench_c17(self):
+        bench_file_out = "./testFiles/c17.bench"
+        input_wire_in = "./testFiles/c17.pi"
+        expected_output_file = "./testFiles/c17.log"
+
+        output_file = "c17.log"
+
+        phaseOne.run_bench(bench_file_out, input_wire_in, output_file)
+
+        with open(expected_output_file, "r") as f:
+            expected_bench = f.read()
+
+        with open(output_file, "r") as f:
+            actual_bench = f.read()
+
+        self.assertEqual(actual_bench.strip(), expected_bench.strip())
+
         os.remove(output_file)
 
 
